d5045cc096f87c5090531c0f11e9eae9d66a9593
Janek Ostendorf <janek@ostendorf.io>
2021-11-29 20:27:27 +0100

    It works! ... when the app is running

--- app/src/main/java/io/ostendorf/heartratetoweb/MainActivity.kt
+++ app/src/main/java/io/ostendorf/heartratetoweb/MainActivity.kt
@@ -3,6 +3,7 @@ package io.ostendorf.heartratetoweb
 import android.Manifest
 import android.app.Activity
 import android.content.Context
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -10,8 +11,12 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
 import com.android.volley.RequestQueue
 import com.android.volley.request.StringRequest
 import com.android.volley.toolbox.Volley
@@ -21,15 +26,23 @@ import kotlin.math.roundToInt
 class MainActivity : Activity(), SensorEventListener {
 
     object Config {
-        const val HTTP_POST_URL = "http://192.168.178.182:6547/"
+        const val CONF_HTTP_HOSTNAME = "HTTP_HOSTNAME"
+        const val CONF_HTTP_PORT = "HTTP_PORT"
+
+        const val CONF_HTTP_HOSTNAME_DEFAULT = ""
+        const val CONF_HTTP_PORT_DEFAULT = 6547
     }
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var mSensorManager: SensorManager
     private lateinit var mHeartRateSensor: Sensor
-    private lateinit var textView: TextView
+    private lateinit var textCurrentHr: TextView
 
     private lateinit var httpQueue: RequestQueue
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    private var runningState: RunningState = RunningState.STOPPED
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,10 +50,15 @@ class MainActivity : Activity(), SensorEventListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        textView = findViewById(R.id.text)
+        textCurrentHr = findViewById(R.id.textViewHeartRate)
 
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE)
+
+        initConfig()
+        bindConfigToInputs()
+
+        textCurrentHr.text = resources.getString(R.string.text_current_hr, 0)
 
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -52,8 +70,63 @@ class MainActivity : Activity(), SensorEventListener {
 
         httpQueue = Volley.newRequestQueue(this)
 
-        startMeasure()
+        wireButton()
 
+    }
+
+    private fun wireButton() {
+        val buttonInput = findViewById<Button>(R.id.buttonStartStop)
+
+        buttonInput.setOnClickListener {
+            if (runningState == RunningState.STOPPED) {
+                runningState = RunningState.RUNNING
+                buttonInput.setText(R.string.button_stop)
+                startMeasure()
+            } else if (runningState == RunningState.RUNNING) {
+                runningState = RunningState.STOPPED
+                buttonInput.setText(R.string.button_start)
+                stopMeasure()
+            }
+        }
+    }
+
+    private fun bindConfigToInputs() {
+        val hostnameInput = findViewById<EditText>(R.id.editTextHostname)
+        val portInput = findViewById<EditText>(R.id.editTextPort)
+
+        hostnameInput.setText(sharedPreferences.getString(Config.CONF_HTTP_HOSTNAME, Config.CONF_HTTP_HOSTNAME_DEFAULT))
+        portInput.setText(sharedPreferences.getInt(Config.CONF_HTTP_PORT, Config.CONF_HTTP_PORT_DEFAULT).toString())
+
+        hostnameInput.doAfterTextChanged {
+            with(sharedPreferences.edit()) {
+                putString(Config.CONF_HTTP_HOSTNAME, it.toString())
+                apply()
+                Log.d("config", "Saved new Hostname: " + it.toString())
+            }
+        }
+
+        portInput.doAfterTextChanged {
+            with(sharedPreferences.edit()) {
+                putInt(Config.CONF_HTTP_PORT, it.toString().toInt())
+                apply()
+                Log.d("config", "Saved new Port: " + it.toString())
+            }
+        }
+    }
+
+    private fun initConfig() {
+        sharedPreferences = this.getPreferences(MODE_PRIVATE)
+
+        with(sharedPreferences.edit()) {
+            if (!sharedPreferences.contains(Config.CONF_HTTP_HOSTNAME)) {
+                putString(Config.CONF_HTTP_HOSTNAME, Config.CONF_HTTP_HOSTNAME_DEFAULT)
+            }
+
+            if (!sharedPreferences.contains(Config.CONF_HTTP_PORT)) {
+                putInt(Config.CONF_HTTP_PORT, Config.CONF_HTTP_PORT_DEFAULT)
+            }
+            apply()
+        }
     }
 
     private fun startMeasure() {
@@ -73,8 +146,8 @@ class MainActivity : Activity(), SensorEventListener {
         val mHeartRateFloat: Float = event!!.values[0]
 
         val mHeartRate: Int = mHeartRateFloat.roundToInt()
-        textView.text = mHeartRate.toString()
-        Log.d("HR: ", mHeartRate.toString());
+        textCurrentHr.text = resources.getString(R.string.text_current_hr, mHeartRate)
+        Log.d("HR: ", mHeartRate.toString())
 
         sendHeartRate(mHeartRate)
     }
@@ -85,9 +158,13 @@ class MainActivity : Activity(), SensorEventListener {
 
     private fun sendHeartRate(heartrate: Int) {
 
+        val httpUrl = "http://" +
+                sharedPreferences.getString(Config.CONF_HTTP_HOSTNAME, Config.CONF_HTTP_HOSTNAME_DEFAULT) +
+                ":" + sharedPreferences.getInt(Config.CONF_HTTP_PORT, Config.CONF_HTTP_PORT_DEFAULT).toString()
+
         val httpRequest = object : StringRequest(
             Method.POST,
-            Config.HTTP_POST_URL,
+            httpUrl,
             { response -> Log.d("HTTP Reponse: ", response) },
             { Log.e("HTTP Error", it.message.toString()) }
         ) {
